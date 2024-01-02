eff59526ec5ca9cb159dcedb5e65012de3db7d5f
KavithRanch <kavithranch@gmail.com>
2024-01-02 09:55:38 -0500

    My implementation of insertion sort (intuition)

--- algorithms/sorting/insertion_sort.py
+++ algorithms/sorting/insertion_sort.py
@@ -5,10 +5,18 @@ def insertion_sort(array):
     arr = array.copy()
     n = len(arr)
     for i in range(n):
-        for j in range(i, 0, -1):
-            if arr[i] >= arr[j]:
-                break
-            else:
+        curIndex = i
+        for j in range(i, -1, -1):
+            if curIndex != j:
+                if arr[curIndex] >= arr[j]:
+                    break
+                else:
+                    temp = arr[curIndex]
+                    arr[curIndex] = arr[j]
+                    arr[j] = temp
+                    curIndex = j
+
+    return arr
 
 
 
--- algorithms/sorting/testing_sort_algorithms.py
+++ algorithms/sorting/testing_sort_algorithms.py
@@ -1,6 +1,7 @@
 import random
 import numpy as n
 from bubble_sort import bubble_sort
+from insertion_sort import insertion_sort
 
 
 def test_bubble(array):
@@ -14,9 +15,20 @@ def test_bubble(array):
     print("The array is sorted correctly") if (n.array(result) == n.array(expected)).all() else print("The array is not sorted correctly")
 
 
+def test_insertion(array):
+    print("Original Array: " + str(array))
+    result = insertion_sort(array)
+    print("Sorted Array: " + str(result))
+
+    expected = array.copy()
+    expected.sort()
+
+    print("The array is sorted correctly") if (n.array(result) == n.array(expected)).all() else print("The array is not sorted correctly")
+
+
 length = 20  # Set this value to whatever you wish
 
 arr = []
 for i in range(length):
     arr.append(random.randint(1, 100))
-test_bubble(arr)
+test_insertion(arr)
